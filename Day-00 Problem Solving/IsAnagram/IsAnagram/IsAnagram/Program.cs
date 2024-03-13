f4b6b818f0bf65594afb0a26cf7801226c184328
fadyehabamer <fadyamer45@gmail.com>
2024-03-13 17:20:28 +0200

    Finalize programs

---
+++ Day-00 Problem Solving/IsAnagram/IsAnagram/IsAnagram/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IsAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IsAnagram isAnagram = new IsAnagram();
+            
+            isAnagram.IsAnagramFunc("anagram", "nagaram");
+            
+        }
+    }
+}
